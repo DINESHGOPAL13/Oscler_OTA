3dc99c323d60698d2dc8deae381d2651cd6d0b3f
DINESHGOPAL13 <73527717+DINESHGOPAL13@users.noreply.github.com>
2023-05-14 23:03:13 +0100

    Initial commit

---
+++ test_Oscler_OTA/Task_1_OTA_main_prototype_code.py
@@ -0,0 +1,135 @@
+import logging
+import struct
+import os
+
+''' 
+->Deployed the firmware.bin image file to my local Tomcat webserver and made the local server active so that 
+firmware can download from below deployed URL
+->URL = 'http://localhost:8080/OTA/firmware.bin 
+->Trying to hit the URL and download the file and place it on workspace then check the download file exists or not
+->Based on that will perform the version compatibility and then finally update the firmware to device successfully
+ 
+->And Included the ota.log file with workspace for to track the execution flow with log'''
+
+import requests
+
+# Constants (Based on firmware image bytes mentioned on table)
+
+
+FIRMWARE_IMAGE_OFFSET = 6
+
+#log file configuration
+logging.basicConfig(filename='ota.log',level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# predefined Functions from production code
+def connect(URL = 'http://localhost:8080/OTA/firmware.bin'):
+
+    filename = 'firmware.bin'
+
+    global response
+    try:
+        response = requests.get(URL)
+
+        if response.status_code == 200:
+            print('Successfully connected to Server Portal')
+            logging.info('Successfully connected to Server Portal')
+            return True
+        else:
+            print('Unable to connect to Server Portal')
+            logging.info('Unable to connect to Server Portal')
+            return False
+    except requests.exceptions.RequestException as e:
+        print(f'Error while connecting to Server Portal: {e}')
+        logging.info(f'Error while connecting to Server Portal: {e}')
+        return False
+
+
+def retrieve_firmware_available():
+    return (2, 0)
+
+
+def get_device_firmware_revision():
+    return (1, 0)
+
+
+# In this function will download and open the firmware file in binary mode
+def download_firmware():
+    filename = 'firmware.bin'
+    try:
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        # check if the file exists on working directory
+        if os.path.exists(filename):
+            print(f'{filename} image downloaded successfully')
+            logging.info(f'{filename} image downloaded successfully')
+            return True
+        else:
+            print(f'Error: {filename} image was not downloaded')
+            logging.info(f'Error: {filename} image was not downloaded')
+            return False
+    except IOError as e:
+        logging.error(f'Error while downloading firmware: {e}')
+        return False
+
+
+def get_device_hardware_revision():
+    return (4, 6)
+
+
+def version_compatible(latest_version, device_version):
+    """Check device version compatible (latest firmware device settings version need to be lessthan or equal to
+     current device version) """
+    device_version = get_device_hardware_revision()
+    latest_version = retrieve_firmware_available()
+
+    if latest_version[0] <= device_version[0]:
+
+        if latest_version[1] <= device_version[1]:
+            return True
+
+    return False
+
+
+def program_firmware(firmware_image):
+    return True
+
+
+def reboot():
+    print('Device is Rebooted successfully')
+    logging.info('Device is Rebooted successfully')
+    pass
+
+
+# Main code for OTA sequence flow
+if __name__ == '__main__':
+    if connect():
+        print('Connected to local tomcat server')
+        logging.info('Connected to local tomcat server')
+        latest_firmware = retrieve_firmware_available()
+        current_firmware = get_device_firmware_revision()
+        if latest_firmware > current_firmware:
+            firmware_image = download_firmware()
+            if firmware_image:
+                print('OTA update is available')
+                logging.info('OTA update is available')
+                hardware_revision = get_device_hardware_revision()
+                # if latest_firmware <= hardware_revision:
+                if version_compatible(latest_firmware, hardware_revision):
+
+                    if program_firmware(firmware_image):
+                        reboot()
+
+                        print('OTA firmware is Updated Successfully to device')
+                        logging.info('OTA firmware is Updated Successfully to device')
+                else:
+                    print('OTA update failed due to incompatible hardware revision')
+                    logging.error('OTA update failed: Incompatible hardware revision')
+            else:
+                print('Failed to download firmware')
+                logging.error('OTA update failed: Failed to download firmware')
+        else:
+            print('No OTA update available')
+            logging.info('OTA update not available')
+    else:
+        print('Failed to connect to device')
+        logging.error('OTA update failed: Failed to connect to device')
---
+++ test_Oscler_OTA/Task_2_testing_of_main_code.py
@@ -0,0 +1,93 @@
+import os
+import unittest
+import requests
+# imports functions from prototype class for testing
+
+from test_Oscler_OTA.Task_1_OTA_main_prototype_code import (
+    connect,
+    retrieve_firmware_available,
+    get_device_firmware_revision,
+    download_firmware,
+    get_device_hardware_revision,
+    version_compatible,
+    program_firmware,
+    reboot,
+)
+
+
+class TestOTA(unittest.TestCase):
+
+    def test_connect_success(self):
+        """Here Actual valid End-Point URL is passed so the test need to connect and pass successfully
+        EXPECTED RESULT: Not Null and 'Successfully connected to Server Portal'
+        ACTUAL RESULT: Not Null and 'Successfully connected to Server Portal'
+        """
+        response = connect('http://localhost:8080/OTA/firmware.bin')
+        '''Check the connection is made or not'''
+        self.assertTrue(response)
+        '''Check the response object return response and its not null'''
+        self.assertIsNotNone(response)
+
+    def test_connect_Failure_1_invalid_url(self):
+        """Here passed the invalid URL so the test need to log the connection error
+        EXPECTED RESULT:Not Null and Error Unable to connect to device
+        ACTUAL RESULT: Not Null and Error Unable to connect to device """
+        response = connect('http://localhost:8080/OTA/firmware.bin12£$%%%%%')
+        '''Check the Expected error is returned or not'''
+        self.assertFalse(response)
+        '''Check the Error response is returned ,not null pre-defined error value is returned or not'''
+        self.assertTrue(response is not None)
+
+    def test_connect_Failure_2_empty_url(self):
+        """Here passed the Null value to the URL so the test need to log the connection error
+        EXPECTED RESULT:Not Null and Error while connecting to device: Invalid URL ''
+        ACTUAL RESULT: Not Null and Error while connecting to device: Invalid URL '' """
+        response = connect('')
+        '''Check the Expected error is returned or not'''
+        self.assertFalse(response)
+        '''Check the Error response is returned ,not null pre-defined error value is returned or not'''
+        self.assertTrue(response is not None)
+
+    def test_connect_Failure_3_Server_error(self):
+        """Here mocked the server error so I have stopped the Deployed service on Tomcat server
+          the test need to log the connection error
+
+        EXPECTED RESULT:Not Null and Error while connecting to Server Portal: HTTPConnection connection Error''
+
+        ACTUAL RESULT: Not Null and Error while connecting to Server Portal: HTTPConnectionPool(host='localhost', port=8080):
+        Max retries exceeded with url: /OTA/firmware.bin (Caused by NewConnectionError('<urllib3.connection.HTTPConnection object at 0x0000016788DF8460>: Failed to establish a new connection: [WinError 10061] No connection could be made because the target machine actively refused it')) '' """
+        response = connect('http://localhost:8080/OTA/firmware.bin')
+        '''Check the Expected error is returned or not'''
+        self.assertFalse(response)
+        '''Check the Error response is returned ,not null pre-defined error value is returned or not'''
+        self.assertTrue(response is not None)
+
+    def test_retrieve_firmware_available_success(self):
+        """Test retrieval of available firmware"""
+        response = retrieve_firmware_available()
+        self.assertTrue(response)
+        self.assertTrue(len(response) > 0)
+
+    def test_download_firmware_verify_file_exist(self):
+        """Test that firmware is downloaded successfully and saved to folder"""
+        # downloaded firmware file path
+        firmware_file_path = 'C://Users//DineshGopal//Documents//Oscler_OTA//test_Oscler_OTA//firmware.bin'
+        self.assertTrue(os.path.exists(firmware_file_path))
+
+    def test_download_firmware_verify_file_non_exist(self):
+        """Test that firmware is not downloaded and saved to folder (before download stage)
+         Verify non-occurrence of file in folder"""
+        # firmware file path
+        firmware_file_path = 'C://Users//DineshGopal//Documents//Oscler_OTA//test_Oscler_OTA//firmware.bin'
+        self.assertFalse(os.path.exists(firmware_file_path))
+
+    def test_download_firmware_verify_file_name(self):
+        """Test that firmware is  downloaded and saved to folder and
+         Verify file name in folder"""
+        # downloaded firmware file path
+        firmware_file_path = 'C://Users//DineshGopal//Documents//Oscler_OTA//test_Oscler_OTA//firmware.bin'
+        self.assertEqual(os.path.basename(firmware_file_path),'firmware.bin')
+
+
+if __name__ == '__main__':
+    unittest.main()
